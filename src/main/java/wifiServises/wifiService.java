731abf59f5bbda26e60cc0cf542bbade89a57d6f
pjy3012 <ppp0868@naver.com>
2022-06-14 19:10:48 +0900

    eclipes로 SQLite에서 데이터 읽어오기 기능 테스트

--- src/main/java/wifiServises/wifiService.java
+++ src/main/java/wifiServises/wifiService.java
@@ -3,32 +3,34 @@ package wifiServises;
 import java.sql.*;
 
 public class wifiService {
-	
-	 Connection con = null;
 
-     try {
+	public void selectWifi() {
 
-         Class.forName("org.sqlite.JDBC");
+		Connection con = null;
 
-         String dpFile = "/Users/junyongpark/Desktop/eclipes-workspace/seoul_wifi/wifiDb/wifiTest";
-         con = DriverManager.getConnection("jdbc:sqlite:" + dpFile);
+		try {
 
-         Statement stat = con.createStatement();
-         ResultSet rs = stat.executeQuery("SELECT X_SWIFI_MGR_NO from seoul_wifi");
-         while(rs.next()) {
-             String MGR_NO = rs.getString(X_SWIFI_MGR_NO);
+			Class.forName("org.sqlite.JDBC");
 
-             System.out.println("와이파이 관리 번호:" + MGR_NO);
-         }
-     } catch (Exception e) {
-         e.printStackTrace();
-     } finally {
-         if (con != null) {
-             try {
-                 con.close();
-             } catch (Exception e) {}
-         }
-     }
- }
+			String dpFile = "/Users/junyongpark/Desktop/eclipes-workspace/seoul_wifi/wifiDb/wifiTest";
+			con = DriverManager.getConnection("jdbc:sqlite:" + dpFile);
 
+			Statement stat = con.createStatement();
+			ResultSet rs = stat.executeQuery("SELECT X_SWIFI_MGR_NO from seoul_wifi");
+			while (rs.next()) {
+				String MGR_NO = rs.getString("X_SWIFI_MGR_NO");
+
+				System.out.println("와이파이 관리 번호:" + MGR_NO);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+	}
 }
--- src/main/java/wifiServises/wifiTest.java
+++ src/main/java/wifiServises/wifiTest.java
@@ -3,5 +3,9 @@ package wifiServises;
 public class wifiTest {
 	public static void main(String[] args) {
 		
+		wifiService wifi = new wifiService();
+		
+		wifi.selectWifi();
+		
 	}
 }
